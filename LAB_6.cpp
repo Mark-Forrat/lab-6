dd3a8cc4b41739d63726bef51b2abd4a5ab045b1
Марк Форрат <112650673+Mark-Forrat@users.noreply.github.com>
2022-12-23 01:10:59 +0500

    Add files via upload

---
+++ LAB_6.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите строку ";
+    string s;
+    getline(cin, s);
+    for (string::iterator i = s.begin(); i != s.end(); ++i)// переход в другой тип
+        if (ispunct(*i))
+        {
+            *i = ' ';
+        }
+    sort(s.begin(), s.end());
+    cout << s;
+    return 0;
+}
